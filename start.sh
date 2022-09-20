19655cf64235ba33a3da00f515b57be9f807db1d
Leonardo Araujo Pimentel <leonardociberxon@gmail.com>
2022-09-20 11:09:49 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-query=$(curl https://api.papermc.io/v2/projects/paper/versions/1.19/builds)
-projectVersion=$(echo $query | jq -r '.version')
-buildNumber=$(echo $query | jq -r '.builds[-1].build')
-fileName=$(echo $query | jq -r '.builds[-1].downloads.application.name')
 
 
 read -r -p "Atualizar Geyser? [y/N] " response
@@ -11,7 +7,7 @@ case "$response" in
 [yY][eE][sS]|[yY]) 
     cd plugins
     rm -rf Geyser-Spigot.jar
-    wget https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
+    wget https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/build/libs/Geyser-Spigot.jar
     cd ..
     ;;
     *)
@@ -20,7 +16,12 @@ esac
 
 read -r -p "Atualizar Paper? [y/N] " response
 case "$response" in
-[yY][eE][sS]|[yY]) 
+[yY][eE][sS]|[yY])
+    query=$(curl https://api.papermc.io/v2/projects/paper/versions/1.19.2/builds)
+    projectVersion=$(echo $query | jq -r '.version')
+    buildNumber=$(echo $query | jq -r '.builds[-1].build')
+    fileName=$(echo $query | jq -r '.builds[-1].downloads.application.name')
+
     rm -rf paper.jar
     wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/$projectVersion/builds/$buildNumber/downloads/$fileName
     ;;
@@ -28,5 +29,5 @@ case "$response" in
     ;;
 esac
 
-java -Xmx14G -Xms3G -jar paper.jar -o true
+java -Xmx15G -Xms3G -jar paper.jar -o true
 
